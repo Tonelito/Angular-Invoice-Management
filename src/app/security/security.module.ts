0eb6f98e3c5c0f159d0232a6b9cd6d709de1b795
tone <jgaliciamazariegos@outlook.es>
2024-09-15 20:12:12 -0600

    login UI Responsive :D

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -1,12 +1,29 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgIf,
+    MatIconModule
+  ],
+  exports: [LoginComponent]
 })
-export class SecurityModule { }
+export class SecurityModule {}
