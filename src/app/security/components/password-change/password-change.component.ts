760778fa18b0ceb9d78a71961fed307ef73e1685
asanchez2019130 <asanchez-2019130@kinal.edu.gt>
2024-09-20 16:09:17 -0600

    Buttons and Validator in password-change

--- src/app/security/components/password-change/password-change.component.ts
+++ src/app/security/components/password-change/password-change.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { confirmPasswordValidator } from 'src/app/shared/utilities/confirm-password.validator';
 
 @Component({
   selector: 'app-password-change',
   templateUrl: './password-change.component.html',
   styleUrls: ['./password-change.component.scss']
 })
-export class PasswordChangeComponent {
+export class PasswordChangeComponent implements OnInit {
 
   hideCurrentPassword = true;
   hideNewPassword = true;
-  
   confirmNewPassword = true;
+  passwordChangeForm!: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.passwordChangeForm = this._formBuilder.group({
+      currentPassword: ['', [Validators.required]],
+      newPassword: new FormControl<string>('', [Validators.required]),
+      newConfirmPassword: new FormControl<string>('', [Validators.required])
+    }, { validators: confirmPasswordValidator });
+  };
 
   toggleCurrentPasswordVisibility() {
     this.hideCurrentPassword = !this.hideCurrentPassword;
@@ -19,7 +31,7 @@ export class PasswordChangeComponent {
   toggleNewPasswordVisibility() {
     this.hideNewPassword = !this.hideNewPassword;
   }
-  
+
   toggleConfirmNewPasswordVisibility() {
     this.confirmNewPassword = !this.confirmNewPassword;
   }
